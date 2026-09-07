9392beb9ed9e2739ec690d3170079ff02cae0bed
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-09-07 14:50:40 +0300

    Add NewPool for Postgres

--- cmd/todo-list/main.go
+++ cmd/todo-list/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os/signal"
 	"todo-list/internal/config"
+	"todo-list/internal/postgres"
 )
 
 func main() {
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("load config: %v", err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background())
+	defer cancel()
+
+	pool, err = postgres.NewPool(ctx, &cfg.Postgres)
+	if err != nil {
+		log.Fatalf("pool creation: %v", err)
 	}
 }
---
+++ internal/postgres/pool.go
@@ -0,0 +1,27 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"todo-list/internal/config"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, cfg *config.PostgresConfig) (*pgxpool.Pool, error) {
+	connectionStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode-disable",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database)
+
+	pool, err := pgxpool.New(ctx, connectionStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	return pool, nil
+}
