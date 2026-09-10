5df0bb512c698998af9a5b4e1e601290723320c5
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-09-10 16:38:18 +0300

    Add TodoService

--- internal/repository/todo.go
+++ internal/repository/todo.go
@@ -1,15 +1,16 @@
 package repository
 
 import (
+	"context"
 	"todo-list/internal/domain"
 
 	"github.com/google/uuid"
 )
 
 type TodoRepo interface {
-	AddTodo(todo *domain.Todo) error
-	GetTodo(id uuid.UUID) (*domain.Todo, error)
-	GetAllTodos() ([]domain.Todo, error)
-	UpdateTodo(todo *domain.Todo) error
-	DeleteTodo(id uuid.UUID) error
+	AddTodo(todo *domain.Todo, ctx context.Context) (*domain.Todo, error)
+	GetTodo(id uuid.UUID, ctx context.Context) (*domain.Todo, error)
+	GetAllTodos(ctx context.Context) ([]domain.Todo, error)
+	UpdateTodo(id uuid.UUID, todo *domain.Todo, ctx context.Context) (*domain.Todo, error)
+	DeleteTodo(id uuid.UUID, ctx context.Context) error
 }
---
+++ internal/service/todo.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+	"todo-list/internal/domain"
+	"todo-list/internal/repository"
+
+	"github.com/google/uuid"
+)
+
+type TodoService struct {
+	todoRepo repository.TodoRepo
+}
+
+func NewTodoService(todoRepo repository.TodoRepo) *TodoService {
+	return &TodoService{todoRepo: todoRepo}
+}
+
+func (s *TodoService) CreateTodo(todo *domain.Todo, ctx context.Context) (*domain.Todo, error) {
+	return s.todoRepo.AddTodo(todo, ctx)
+}
+
+func (s *TodoService) GetTodo(id uuid.UUID, ctx context.Context) (*domain.Todo, error) {
+	return s.todoRepo.GetTodo(id, ctx)
+}
+
+func (s *TodoService) GetAllTodos(ctx context.Context) ([]domain.Todo, error) {
+	return s.todoRepo.GetAllTodos(ctx)
+}
+
+func (s *TodoService) UpdateTodo(id uuid.UUID, todo *domain.Todo, ctx context.Context) (*domain.Todo, error) {
+	return s.todoRepo.UpdateTodo(id, todo, ctx)
+}
+
+func (s *TodoService) DeleteTodo(id uuid.UUID, ctx context.Context) error {
+	return s.todoRepo.DeleteTodo(id, ctx)
+}
